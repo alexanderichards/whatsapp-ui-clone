29b729758652516b0c83155f33b2dd10dc4c1a84
Alexander Richards <alexanderichards@hotmail.com>
2020-09-23 19:13:02 -0500

    Scroll to the end of the chat

--- src/components/MessagesBox.js
+++ src/components/MessagesBox.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import Message from './Message'
 
 function MessagesBox({messages}) {
+    const endDiv = useRef(null)
+    
+    useEffect(() => {
+        endDiv.current.scrollIntoView()
+    })
     return (
         <div className="chats">
             {messages.map((message) => (
                 <Message message={message} key={message.id}></Message>
             ))};
+            <div style={{float: "right", clear: 'both'}} ref={endDiv}></div>
         </div>
     );
 }
